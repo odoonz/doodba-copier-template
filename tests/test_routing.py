8d9dfa99a4cb9f09a78611d79e71a3f4bd74a2fa
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-08-20 10:05:44 +0100

    fixup! fixup! Modify multidomains structure

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,5 +1,6 @@
 import time
 import uuid
+from itertools import product
 from pathlib import Path
 
 import requests
@@ -16,20 +17,28 @@ def test_multiple_domains_prod(
 ):
     """Test multiple domains are produced properly."""
     data = {
-        "traefik_cert_resolver": None,
         "odoo_version": supported_odoo_version,
         "project_name": uuid.uuid4().hex,
-        "domains_prod": {
-            f"main.{traefik_host}.sslip.io": [
-                f"alt0.main.{traefik_host}.sslip.io",
-                f"alt1.main.{traefik_host}.sslip.io",
-            ],
-            f"secondary.{traefik_host}.sslip.io": [
-                f"alt0.secondary.{traefik_host}.sslip.io",
-                f"alt1.secondary.{traefik_host}.sslip.io",
-            ],
-            f"third.{traefik_host}.sslip.io": [],
-        },
+        "domains_prod": [
+            # main0 has no TLS
+            {"hosts": [f"main0.{traefik_host}.sslip.io"], "cert_resolver": None},
+            {
+                "hosts": [
+                    f"alt0.main0.{traefik_host}.sslip.io",
+                    f"alt1.main0.{traefik_host}.sslip.io",
+                ],
+                "cert_resolver": None,
+            },
+            # main1 has self-signed certificates
+            {"hosts": [f"main1.{traefik_host}.sslip.io"], "cert_resolver": True},
+            {
+                "hosts": [
+                    f"alt0.main1.{traefik_host}.sslip.io",
+                    f"alt1.main1.{traefik_host}.sslip.io",
+                ],
+                "cert_resolver": True,
+            },
+        ],
     }
     dc = docker_compose["-f", "prod.yaml"]
     with local.cwd(tmp_path):
@@ -47,12 +56,18 @@ def test_multiple_domains_prod(
             )
             dc("up", "-d")
             time.sleep(10)
-            for main_domain, alt_list in data["domains_prod"].items():
-                for alt_domain in alt_list + [main_domain]:
-                    response = requests.get(f"http://{alt_domain}/web/login")
-                    assert response.ok
-                    assert response.url == f"http://{main_domain}/web/login"
+            for main_num, alt_num in product(range(2), range(2)):
+                http = "http" if main_num == 0 else "https"
+                main_domain = f"main{main_num}.{traefik_host}.sslip.io"
+                alt_domain = f"alt{alt_num}.{main_domain}"
+                response = requests.get(f"http://{alt_domain}/web/login", verify=False)
+                assert response.ok
+                assert response.url == f"{http}://{main_domain}/web/login"
             bad_response = requests.get(f"http://missing.{traefik_host}.sslip.io")
+            assert bad_response.status_code == 404
+            bad_response = requests.get(
+                f"https://missing.{traefik_host}.sslip.io", verify=False
+            )
             assert bad_response.status_code == 404
         finally:
             dc("down", "--volumes", "--remove-orphans")
